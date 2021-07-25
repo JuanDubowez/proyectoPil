0712cf1a2e92a45967f8f6e441e5ed2fb997c131
fecherico <federicoruizdiaz91@gmail.com>
2021-07-25 17:47:55 -0300

    cambios y estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
-  {path: '', redirectTo: '/sign-up', pathMatch: 'full'}
+  {path: '', component: LandingPageComponent},
+
   ];
 
 @NgModule({
