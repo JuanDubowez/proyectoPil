f690acf89b701aa209948a437ed7554309604301
fecherico <federicoruizdiaz91@gmail.com>
2021-07-29 00:42:14 -0300

    agrego component menu sidebar al proyecto

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule, AppRoutingModule
   ],
-  exports: [NavbarComponent, FooterComponent]
+  exports: [NavbarComponent, FooterComponent,MenuComponent]
 })
 export class LayoutModule { }
---
+++ src/app/layout/menu/menu.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+openNav() {
+// let v=document.getElementById("mySidebar").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+}
+
+// closeNav() {
+//   document.getElementById("mySidebar").style.width = "0";
+//   document.getElementById("main").style.marginLeft= "0";
+// }
+
+
+}
