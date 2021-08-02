ce51ea8e07423bb2072b94168f9890ba35a28a7f
fecherico <federicoruizdiaz91@gmail.com>
2021-08-01 23:33:01 -0300

    nuevos cambios y agrego componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: '', component: LandingPageComponent},
 
